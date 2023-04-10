15a82a8095bbbcbcd19735eb5451f6e4bb79451a
jerry <jerrynwosu007@gmail.com>
2023-04-10 23:24:34 +0100

    update

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,15 @@
-console.log('Hello App Setuper');
+import express, { Application } from 'express';
+
+export class App {
+  private readonly app: Application;
+
+  constructor() {
+    this.app = express();
+  }
+
+  public start() {
+    this.app.listen(8080, () => {
+      console.log(`Server is running on port 8080`);
+    });
+  }
+}
