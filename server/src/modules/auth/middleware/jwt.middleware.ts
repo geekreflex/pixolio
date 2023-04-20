0cada81d7cae7a94db841beccb992f1312f2c6b7
jerry <jerrynwosu007@gmail.com>
2023-04-20 21:36:29 +0100

    check valid refresh token needed

--- server/src/modules/auth/middleware/jwt.middleware.ts
+++ server/src/modules/auth/middleware/jwt.middleware.ts
@@ -2,8 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 import { UserService } from '../../user';
 import { ResponseCode, respond } from '../../../utils/response';
 import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 
-class JwtMiddleware {
+export class JwtMiddleware {
   private userService: UserService;
   private jwtSecret: string = process.env.JWT_SECRET as string;
 
@@ -28,4 +29,32 @@ class JwtMiddleware {
       respond(res, {}, 'Unauthorized', ResponseCode.UNAUTHORIZED);
     }
   }
+
+  public validRefreshNeeded = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const user = await this.userService.getUserByEmailWithPassword(
+      res.locals.jwt.email
+    );
+
+    const salt = crypto.createSecretKey(
+      Buffer.from(res.locals.jwt.refreshKey.data)
+    );
+    const hash = crypto
+      .createHmac('sha512', salt)
+      .update(res.locals.jwt.id + this.jwtSecret)
+      .digest('base64');
+
+    if (hash === req.body.refreshToken) {
+      req.body = {
+        id: user?.id,
+        email: user?.email,
+      };
+      return next();
+    } else {
+      respond(res, {}, 'Invalid refresh token', ResponseCode.BAD_REQUEST);
+    }
+  };
 }
